feed4843d171e9f4f64b0e68e9c69ecd75998a71
Mihails Jakovlevs <mjakovlevs@gmail.com>
2020-04-03 11:04:21 +0300

    deploy fix

--- src/team3/tests/CardinalDirectionsTest.java
+++ src/team3/tests/CardinalDirectionsTest.java
@@ -1,35 +1,36 @@
 package team3.tests;
 
-import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import team3.weatherapis.CardinalDirection;
 
-
 public class CardinalDirectionsTest {
 
 	@Test
-	@DisplayName("CardinalDirectionsTest") 
-	public void testCardinalDirectionsTest () 
+	@DisplayName("CardinalDirectionsTest")
+	public void testCardinalDirectionsTest()
 
 	{
 		assertAll(
-				() -> assertEquals(CardinalDirection.NORTHEAST, CardinalDirection.fromDegree(67.0f ), "Wrong Cardinal Direction "),
-				() -> assertEquals(CardinalDirection.EAST, CardinalDirection.fromDegree(112.0f ), "Wrong Cardinal Direction "),
-				() -> assertEquals(CardinalDirection.SOUTHEAST, CardinalDirection.fromDegree(157.0f ), "Wrong Cardinal Direction "),
-				() -> assertEquals(CardinalDirection.SOUTH, CardinalDirection.fromDegree(202.0f ), "Wrong Cardinal Direction "),
-				() -> assertEquals(CardinalDirection.SOUTHWEST, CardinalDirection.fromDegree(247.0f ), "Wrong Cardinal Direction "),
-				() -> assertEquals(CardinalDirection.WEST, CardinalDirection.fromDegree(292.0f ), "Wrong Cardinal Direction "),
-				() -> assertEquals(CardinalDirection.NORTHWEST, CardinalDirection.fromDegree(337.0f ), "Wrong Cardinal Direction "),
-				() -> assertEquals(CardinalDirection.NORTH, CardinalDirection.fromDegree(357f), "Wrong Cardinal Direction "),
-				() -> assertEquals(CardinalDirection.NONE, CardinalDirection.fromDegree(-1100), "Wrong Cardinal Direction ")
-				);
-	}	
-	
-	
-	
-	
+				() -> assertEquals(CardinalDirection.NORTHEAST, CardinalDirection.fromDegree(67.0f),
+						"Wrong Cardinal Direction "),
+				() -> assertEquals(CardinalDirection.EAST, CardinalDirection.fromDegree(112.0f),
+						"Wrong Cardinal Direction "),
+				() -> assertEquals(CardinalDirection.SOUTHEAST, CardinalDirection.fromDegree(157.0f),
+						"Wrong Cardinal Direction "),
+				() -> assertEquals(CardinalDirection.SOUTH, CardinalDirection.fromDegree(202.0f),
+						"Wrong Cardinal Direction "),
+				() -> assertEquals(CardinalDirection.SOUTHWEST, CardinalDirection.fromDegree(247.0f),
+						"Wrong Cardinal Direction "),
+				() -> assertEquals(CardinalDirection.WEST, CardinalDirection.fromDegree(292.0f),
+						"Wrong Cardinal Direction "),
+				() -> assertEquals(CardinalDirection.NORTHWEST, CardinalDirection.fromDegree(337.0f),
+						"Wrong Cardinal Direction "),
+				() -> assertEquals(CardinalDirection.NORTH, CardinalDirection.fromDegree(357f),
+						"Wrong Cardinal Direction "),
+				() -> assertEquals(CardinalDirection.NONE, CardinalDirection.fromDegree(-1100),
+						"Wrong Cardinal Direction "));
 	}
-
+}
--- src/team3/tests/unitConversionTest.java
+++ src/team3/tests/unitConversionTest.java
@@ -1,7 +1,6 @@
 package team3.tests;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -11,49 +10,42 @@ import team3.weatherapis.WeatherApi;
 
 class unitConversionTest {
 
+	@Nested
+	class CelsiusToKelvinTest {
 
+		@Test
+		public void testIsKelvinToCelsiusValidTrue() {
+			assertTrue((WeatherApi.isKelvinToCelsiusValid("50") == true));
+		}
 
-		@Nested
-		class CelsiusToKelvinTest {
+		@Test
+		public void testIsKelvinToCelsiusValidFalse() {
+			assertTrue((WeatherApi.isKelvinToCelsiusValid("-300") == false));
+		}
 
-			@Test
-			public void testIsKelvinToCelsiusValidTrue() {
-				assertTrue((WeatherApi.isKelvinToCelsiusValid("50") == true));
-			}
+		@Test
+		public void testIsValidTemperatureException() {
+			assertTrue((Weather.isValidTemperature("test case") == false));
+		}
+	}
 
-			@Test
-			public void testIsKelvinToCelsiusValidFalse() {
-				assertTrue((WeatherApi.isKelvinToCelsiusValid("-300") == false));
-			}
+	@Nested
+	class kphToMpsTest {
 
-			@Test
-			public void testIsValidTemperatureException() {
-				assertTrue((Weather.isValidTemperature("test case") == false));
-			}
+		@Test
+		public void testIskphToMpsValidTrue() {
+			assertTrue((WeatherApi.iskphToMpsValid("50") == true));
+		}
+
+		@Test
+		public void testIskphToMpsValidTrueFalse() {
+			assertTrue((WeatherApi.iskphToMpsValid("600") == false));
+		}
+
+		@Test
+		public void testIskphToMpsValidTrueException() {
+			assertTrue((WeatherApi.iskphToMpsValid("test case") == false));
+		}
+
+	}
 }
-		@Nested
-		class kphToMpsTest {
-
-			@Test
-			public void testIskphToMpsValidTrue() {
-				assertTrue((WeatherApi.iskphToMpsValid("50") == true));
-			}
-
-			@Test
-			public void testIskphToMpsValidTrueFalse() {
-				assertTrue((WeatherApi.iskphToMpsValid("600") == false));
-			}
-
-			@Test
-			public void testIskphToMpsValidTrueException() {
-				assertTrue((WeatherApi.iskphToMpsValid("test case") == false));
-			}
-	
-}
-}
-		
-		
-		
-		
-		
-
